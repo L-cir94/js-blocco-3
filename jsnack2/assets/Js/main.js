d67120ea951bf3b00e814e2490d0f2cc2f37ec3e
L-cir94 <luca.cirigliano94@hotmail.it>
2023-03-23 12:46:41 +0100

    attempt

--- jsnack2/assets/Js/main.js
+++ jsnack2/assets/Js/main.js
@@ -1,113 +1,18 @@
 
 /* consegna
-Crea un array composto da 10 automobili.
-Ogni oggetto automobile avrà le seguenti proprietà: marca, 
-modello e alimentazione (benzina, diesel, gpl, elettrico, metano).
-Dividi le automobili in 3 array separati: nel primo array solo le auto 
-a benzina, nel secondo solo le auto a diesel, nel terzo il resto delle auto.
-Infine stampa separatamente i 3 array.
+A partire da un array di stringhe, crea un secondo array 
+formattando le stringhe del primo array in minuscolo e con l’iniziale maiuscola.
 */
 
 /* strumenti: 
 
 */
-let cars = [
-    {
-        "marca": "Fiat",
-        "modello": "Punto",
-        "alimentazione": "benzina"
-    },
-    {
-        "marca": "Audi",
-        "modello": "A8",
-        "alimentazione": "benzina"
-    },
-    {
-        "color": "Audi",
-        "modello": "q-2",
-        "alimentazione": "benzina"
-    },
-        {
-        "marca": "Fiat",
-        "modello": "Punto",
-        "alimentazione": "metano"
-    },
-    {
-        "marca": "Audi",
-        "modello": "A8",
-        "alimentazione": "metano"
-    },
-    {
-        "color": "Audi",
-        "modello": "q-2",
-        "alimentazione": "metano"
-    },
-        {
-        "marca": "Fiat",
-        "modello": "Punto",
-        "alimentazione": "elettrico"
-    },
-    {
-        "marca": "Audi",
-        "modello": "A8",
-        "alimentazione": "elettrico"
-    },
-    {
-        "color": "Audi",
-        "modello": "q-2",
-        "alimentazione": "elettrico"
-    },
-        {
-        "marca": "Fiat",
-        "modello": "Punto",
-        "alimentazione": "gpl"
-    },
-    {
-        "marca": "Audi",
-        "modello": "A8",
-        "alimentazione": "gpl"
-    },
-    {
-        "color": "Audi",
-        "modello": "q-2",
-        "alimentazione": "gpl"
-    },
-        {
-        "marca": "Fiat",
-        "modello": "Punto",
-        "alimentazione": "diesel"
-    },
-    {
-        "marca": "Audi",
-        "modello": "A8",
-        "alimentazione": "diesel"
-    },
-    {
-        "color": "Audi",
-        "modello": "q-2",
-        "alimentazione": "diesel"
-    },
-
-
+let nomi = [
+    "pippo",
+    "PapeRINO",
+    "PADRE",
+    "SilVIO",
+    "AnnA",
 ]
-/* const autoBenza = cars.filter((cars) => {
-  return cars["alimentazione"] === "benzina";
-})
-console.log(autoBenza);
-const autoDiesel = cars.filter((cars) => {
-  return cars["alimentazione"] === "diesel";
-})
-console.log(autoDiesel);
-const altrAlimentazione = cars.filter((cars) => {
-  return cars["alimentazione"] !== "diesel" && cars["alimentazione"] !== "benzina" ;
-})
-console.log(altrAlimentazione) */
 
-//meno codice:
-const autoBenza = cars.filter((cars) => cars ["alimentazione"] === "benzina")
-console.log(autoBenza);
-
-const autoDiesel = cars.filter((cars) => cars ["alimentazione"] === "diesel")
-console.log(autoDiesel);
-const altrAlimentazione = cars.filter((cars) => cars["alimentazione"] !== "diesel" && cars ["alimentazione"] !== "benzina")
-console.log(altrAlimentazione)
+const newArr = nomi.map(nomiGiustificati => nomi.chart(0))
